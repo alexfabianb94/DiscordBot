711fd333cc27c941c8dd7c000c4ccd21b10073b4
Alex Barrales A <alexfabianb94@gmail.com>
2020-08-01 23:16:06 -0400

    modified:   bot.js

--- bot.js
+++ bot.js
@@ -2,15 +2,65 @@
 require('dotenv').config();
 
 const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+const {tuetue, fanswers, audio_response} = require('./constants.js')
 const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+client.on('message', async message => {
+
+  if (!message.guild) return;
+  
+  if (message.content in fanswers) {
+    key = message.content;
+    message.channel.send(fanswers[key].msg, {files: fanswers[key].files});
+  }
+
+  if (message.content in audio_response) {
+    key = message.content;
+    if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+      const dispatcher = connection.play(audio_response[key].audio);
+      if ('files' in audio_response[key]){
+        message.channel.send(audio_response[key].msg, {files: audio_response[key].files})
+      }
+    } else {
+      message.reply('Ponte vío y entra a un canal de audio');
+      message.channel.send(audio_response[key].msg, {files: [audio_response[key].audio]});
+    }
+  }
+
+  if (message.content === '!tuetue') {
+    if (tuetue.count != 2) {
+      tuetue.count += 1
+      message.channel.send(tuetue.icon.repeat(tuetue.count), {files: [tuetue.file]});
+    } else{
+      if (message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+        const dispatcher = connection.play(tuetue.audio);
+      }
+      message.channel.send(tuetue.icon.repeat(3), {files: [tuetue.file]});
+      tuetue.count = 0;
+    }
+  }
+
+  if (message.content === '!help') {
+    help_text = "\nMensaje de Ayuda\n\nGifs\n----------\n"
+    Object.keys(fanswers).forEach(i => {
+      help_text += i + '\n'
+    });
+
+    help_text += "\nAudios\n----------\n"
+    Object.keys(audio_response).forEach(i => {
+      help_text += i + '\n'
+    });
+    message.reply(help_text)
+
+
+    
   }
 });
 
---
+++ constants.js
@@ -0,0 +1,60 @@
+const tuetue = {
+    count: 0,
+    icon: ':clap:',
+    file: "static/images/tuetue.webp",
+    audio: 'static/audio/tuetue.wav'
+  };
+
+const fanswers = {
+    '!telacreiste': {msg: '', files: ['static/images/telacreiste.gif']},
+    '!rosamel': {msg: '', files: ['static/images/rosamel.webp']},
+    '!cataldo': {msg: '', files: ['static/images/cataldo.webp']},
+    '!elsantelices': {msg: '', files: ['static/images/elsantelices.webp']},
+    '!linfati': {msg: '', files: ['static/images/linfati.webp']},
+    '!entroporaca': {msg: '', files: ['static/images/entroporaca.webp', 
+                                        'static/images/salioporaca.webp']},
+    '!jerry': {msg: '', files: ['static/images/jerry.webp']},
+    '!perro': {msg: '', files: ['static/images/perro.webp']},
+    '!chayanne': {msg: '', files: ['static/images/chayanne.webp']},
+}
+
+const tanswers = {
+    '!ayudita': {msg: 'Comandos disponibles: '}
+}
+
+const audio_response = {
+    '!risas': {msg: '', audio: 'static/audio/don-ramon.webm'},
+    '!chipamogli': {msg: '', audio: 'static/audio/chipamogli.webm'},
+    '!platano': {msg: '', audio: 'static/audio/platano.webm'},
+    '!eo': {msg: '', audio: 'static/audio/eo.webm'},
+    '!ladcobra': {msg: '', audio: 'static/audio/lad-cobra.webm',
+                          files: ['https://media.cnnchile.com/2019/03/lad-cobra-06.jpg']},
+    '!panchito': {msg: '', audio: 'static/audio/pancho-saavedra.webm', 
+                            files: ['static/images/pancho-saavedra.jpg']},
+    '!lucho': {msg: '', audio: 'static/audio/lucho.webm'},
+    '!avemaria': {msg: '', audio: 'static/audio/ave-maria.webm'},
+    '!nooo': {msg: '', audio: 'static/audio/noo.mp3'},
+    '!bastalda': {msg: '', audio: 'static/audio/bastalda.webm'},
+    '!publico': {msg: '', audio: 'static/audio/publico.webm'},
+    '!adioschimuelo': {msg: '', audio: 'static/audio/adioschimuelo.webm',
+                                files: ['static/images/juegos-del-hambre.gif']},
+    '!chimuelo': {msg: '', audio: 'static/audio/chimuelo.webm'},
+    '!ilamore': {msg: '', audio: 'static/audio/ilamore.webm'},
+    '!teleton': {msg: '', audio: 'static/audio/teleton.webm'},
+    '!diaplatano': {msg: '', audio: 'static/audio/diaplatano.webm'},
+    '!cerveza': {msg: '', audio: 'static/audio/cerveza.webm'},
+    '!impacto': {msg: '', audio: 'static/audio/impacto.webm'},
+    '!dramatico': {msg: '', audio: 'static/audio/dramatico.webm'},
+    '!hermogenes': {msg: '', audio: 'static/audio/hermogenes.webm'},
+    '!tuetuetue': {msg: '', audio: 'static/audio/tuetue.wav'},
+}
+
+
+
+
+module.exports = {
+    tuetue: tuetue,
+    fanswers: fanswers,
+    audio_response: audio_response,
+    tanswers: tanswers
+};
